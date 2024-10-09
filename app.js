d948eb34a972e6c1e522dddd401610cfc0f213f1
Henrique Heruster de Carvalho <henriquecarvalho@credisan.com.br>
2024-10-09 10:38:03 -0300

    Subindo API sem node_modules

---
+++ app.js
@@ -0,0 +1,132 @@
+const express = require("express");
+const multer = require("multer") 
+const app = express();
+
+const bodyParser = require('body-parser')
+
+const connection = require('./database/database')
+
+const mercadoModel = require('./database/mercados')
+
+const { where } = require("sequelize");
+const { raw } = require("mysql2");
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/img/')
+    },
+    filename: function (req, file, cb) {
+        // Extração da extensão do arquivo original:
+        const extensaoArquivo = file.originalname.split('.')[1];
+
+        // Cria um código randômico que será o nome do arquivo
+        const novoNomeArquivo = require('crypto')
+            .randomBytes(64)
+            .toString('hex');
+
+        // Indica o novo nome do arquivo:
+        cb(null, `${novoNomeArquivo}.${extensaoArquivo}`)
+    }
+});
+ 
+var upload = multer ({dest : 'public/img/'})
+upload =multer({storage})
+
+connection.
+      authenticate().
+      then(()=>{
+         console.log("Conectado ao banco de dados!")
+      })
+      .catch((msgErro)=>{
+         console.log("msgErro")
+})
+
+
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+app.set('view engine','ejs')
+
+app.use(express.static('public'))
+
+
+
+app.get("/cadProdutos/:id", function (req, res) { 
+    res.render("cadProdutos"); 
+}); 
+
+app.get("/cadmercado", function (req, res) { 
+    res.render("cadMercado"); 
+}); 
+
+app.post("/submitMercado", upload.single('img'), function (req, res, next) { 
+    var nome = req.body.nome
+    var img = req.file.path.replace("public", "")
+    console.log(img)
+    
+    mercadoModel.create({
+        nome: nome,
+        img:img
+    }).then(()=>{
+        res.redirect('/')
+        
+    }).catch((error) => {
+        console.error(error);
+        res.status(500).send("Erro ao criar mercado");
+    });
+
+});
+
+app.get("/editarMercado/:id",(req,res)=>{
+    var id = req.params.id
+    mercadoModel.findAll({raw:true, where:{id: id}}).then(mercado=>{
+
+        res.render("editMercado",{
+            mercado : mercado[0]
+        })
+    })
+})
+
+app.post("/submitEditar", async (req, res)=>{
+    var id = req.body.id
+    var nome = req.body.nome
+
+    
+    let registro = await mercadoModel.findOne({ where:{id: id}})
+    registro.nome = nome
+    registro.save().then((i)=>{
+        res.redirect('/')
+    })
+
+})
+
+
+
+app.get("/excluirMercado/:id",(req,res)=>{
+    var id = req.params.id
+    mercadoModel.destroy({
+       where: {
+          id:id
+       }
+    }).then(()=>{
+       res.redirect('back')
+    })
+})
+ 
+app.get("/",(req,res)=>{
+     
+    mercadoModel.findAll({raw:true}).then(mercado =>{
+       res.render('index',{
+          mercado:mercado
+       })
+    })
+    
+ })
+ 
+app.listen(8181, function (erro) { 
+   if (erro) { 
+       console.log("Erro"); 
+   } else { 
+       console.log("Servidor iniciado..."); 
+   } 
+}); 
---
+++ database/mercados.js
@@ -0,0 +1,21 @@
+const sequelize = require('sequelize')
+const connection = require('./database')
+
+const mercados = connection.define('mercados',{
+    nome:{
+        type: sequelize.STRING,
+        allowNull: false
+        //primaryKey: true,
+        //autoIncrement: true
+    },
+    img:{
+        type:sequelize.STRING,
+        allowNull: true
+    }
+})
+
+mercados.sync({force:true}).then(()=>{
+    console.log("Tabela de mercados criada!")
+})
+
+module.exports = mercados
