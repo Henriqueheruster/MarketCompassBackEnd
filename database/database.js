112e9d622fe08e6f356c28983ca48f175648d111
Emanuel Feliciano Urbano <62392361+EmanuelUrbano@users.noreply.github.com>
2024-10-15 20:27:52 -0300

    Update database.js

--- database/database.js
+++ database/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
 
-const connection = new Sequelize('marketcompass','root','1234',{
-    host: 'localhost',
+const connection = new Sequelize('marketcompass','root','HEmafae88562989#NuelBD',{
+    host: 'server-database',
     dialect: 'mysql'
 })
 
